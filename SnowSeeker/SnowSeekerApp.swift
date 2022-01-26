7e2877d640acbb777582dfee15f710268434a9a5
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-01-26 12:58:51 +0300

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Dmitry Sharabin on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
