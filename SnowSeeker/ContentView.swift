e239f0ce500fb8b0eac3fd34cf5d5e894d1eaa67
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-01-26 13:16:29 +0300

    Day 96 — Working with two side by side views in SwiftUI

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        NavigationView {
+//            Text("Hello, world!")
+//                .navigationTitle("Primary")
+//
+//            Text("Secondary")
+//        }
+        
+        NavigationView {
+            NavigationLink {
+                Text("New secondary")
+            } label: {
+                Text("Hello, world!")
+            }
+            .navigationTitle("Primary")
+            
+            Text("Secondary")
+            
+            Text("Tertiary")
+        }
     }
 }
 
