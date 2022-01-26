56a0dfacbea5e5d89a63a0fd9c38d373cfdc7028
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-01-26 13:49:54 +0300

    Day 96 — Using groups as transparent layout containers

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,27 +7,42 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna and Arya")
+        }
+        .font(.title)
+    }
 }
 
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
-    @State private var isShowingUser = false
+//    @State private var layoutVertically = false
+    @Environment(\.horizontalSizeClass) var sizeClass
     
     var body: some View {
-        Text("Hello, world!")
-            .onTapGesture {
-                selectedUser = User()
-                isShowingUser = true
-            }
-//            .sheet(item: $selectedUser) { user in
-//                Text(user.id)
+//        Group {
+//            if layoutVertically {
+//                VStack {
+//                    UserView()
+//                }
+//            } else {
+//                HStack {
+//                    UserView()
+//                }
 //            }
-//            .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser) { user in
-//                Button(user.id) { }
-//            }
-            .alert("Welcome", isPresented: $isShowingUser) { }
+//        }
+//        .onTapGesture {
+//            layoutVertically.toggle()
+//        }
+        
+        if sizeClass == .compact {
+            VStack(content: UserView.init)
+        } else {
+            HStack(content: UserView.init)
+        }
     }
 }
 
